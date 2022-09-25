593b460a6efa5dd7652be9c87dea2820c5f2b760
Muhammad-Sameed-Ansari <52503336+Muhammad-Sameed-Ansari@users.noreply.github.com>
2022-09-25 23:04:27 +0500

    Add Feature to pick image from gallery

--- screens/ModalScreen.js
+++ screens/ModalScreen.js
@@ -1,10 +1,11 @@
-import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity, Button } from 'react-native'
 import React, { useState } from 'react'
 import { images } from '../constants'
 import useAuth from '../hooks/useAuth'
 import { doc, serverTimestamp, setDoc } from 'firebase/firestore'
 import { db } from '../firebase'
 import { useNavigation } from '@react-navigation/native'
+import * as ImagePicker from 'expo-image-picker';
 
 const ModalScreen = () => {
     const navigation = useNavigation()
@@ -12,6 +13,24 @@ const ModalScreen = () => {
     const [image, setImage] = useState(null)
     const [job, setJob] = useState(null)
     const [age, setAge] = useState(null)
+
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
+    const incompleteForm = !image || !job || !age
 
     const updateUserProfile = () => {
         setDoc(doc(db, "users", user.uid), {
@@ -22,13 +41,11 @@ const ModalScreen = () => {
             age: age,
             timestamp: serverTimestamp()
         })
-        .then(() => navigation.replace())
+        .then(() => navigation.goBack())
         .catch((error) => alert(error.message))
 
     }
-
-    const incompleteForm = !image || !job || !age;
-
+    
     return (
         <View style={{ flex: 1, alignItems: 'center', paddingTop: 4 }}>
             <Image 
@@ -40,12 +57,15 @@ const ModalScreen = () => {
             <Text style={styles.welcome}>Welcome {user.displayName}</Text>
         
             <Text style={styles.stepTxt}>Step 1: The Profile Pic</Text>
-            <TextInput 
+            {/* <TextInput 
                 value={image}
                 onChangeText={text => setImage(text)}
                 style={styles.inputTxt}
                 placeholder='Enter a Profile Pic URL'
-            />
+            /> */}
+            <Button title='Gallery' onPress={pickImage}/>
+            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+            
 
             <Text style={styles.stepTxt}>Step 2: The Job</Text>
             <TextInput 
