7e739d22c48814e5a00ccf4d969af69ff699eadd
Muhammad-Sameed-Ansari <52503336+Muhammad-Sameed-Ansari@users.noreply.github.com>
2022-09-28 23:18:55 +0500

    Chat in progress

--- constants/images.js
+++ constants/images.js
@@ -2,10 +2,12 @@ const tinder_login = require('../assets/tinder-login.png')
 const tinder_logo = require('../assets/tinder-logo.png')
 const tinder_logo_2 = require('../assets/tinder-logo-2.png')
 const no_more_profile = require('../assets/no-more-profile.png')
+const its_a_match = require('../assets/its-a-match.png')
 
 export default {
     tinder_login,
     tinder_logo,
     tinder_logo_2,
-    no_more_profile
+    no_more_profile,
+    its_a_match
 }
--- screens/MatchedScreen.js
+++ screens/MatchedScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
+import { images } from '../constants'
 
 const MatchedScreen = () => {
     const navigation = useNavigation();
@@ -10,7 +11,11 @@ const MatchedScreen = () => {
 
     return (
         <View style={styles.container}>
-            <Text>I am a MatchedScreen</Text>
+            <View style={styles.imageContainer}>
+                <Image 
+                    source={images.its_a_match}
+                />
+            </View>
         </View>
     )
 }
@@ -21,6 +26,12 @@ const styles = StyleSheet.create({
     container: {
         height: '100%',
         backgroundColor: 'rgb(239, 68, 68)',
-
+        paddingTop: 80,
+        opacity: 0.89
+    },
+    imageContainer: {
+        justifyContent: 'center',
+        paddingHorizontal: 40,
+        paddingTop: 80
     }
 })
